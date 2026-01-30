758534cf737164d092425f3e4a80ce2558dcdf8a
roberto_esaclear <roberto.delprete@esa.int>
2026-01-30 14:38:04 +0100

    fix: update .gitignore to remove docs directory and adjust SNAP installation script for version 13

--- scripts/snap_installer/install.sh
+++ scripts/snap_installer/install.sh
@@ -26,19 +26,19 @@ if [ "$VERSION" = "12" ]; then
     echo 'Installing SNAP 12...'
     ./esa-snap_all_linux-12.0.0.sh -q -varfile "$(pwd)/snap.varfile" -dir "${SNAP_DIR}"
     echo 'Configuring SNAP memory settings...'
-    echo "-Xmx8G" > "/Data_large/SARGFM/snap/snap/bin/gpt.vmoptions"
+    echo "-Xmx8G" > "${SNAP_DIR}/snap/bin/gpt.vmoptions"
     echo 'SNAP 12 installation complete.'
 
 elif [ "$VERSION" = "13" ]; then
     # VERSION 13 installation
     echo 'Configuring SNAP 13 installation...'
-    curl -O https://download.esa.int/step/snap/12.0/installers/esa-snap_all_linux-12.0.0.sh
+    curl -O https://download.esa.int/step/snap/13.0/installers/esa-snap_sentinel_linux-13.0.0.sh
     chmod +x esa-snap_sentinel_linux-13.0.0.sh
-    echo -e "deleteAllSnapEngineDir\$Boolean=false\ndeleteOnlySnapDesktopDir\$Boolean=false\nexecuteLauncherWithPythonAction\$Boolean=false\nforcePython\$Boolean=false\npythonExecutable=/usr/bin/python\nsys.adminRights\$Boolean=true\nsys.component.RSTB\$Boolean=true\nsys.component.S1TBX\$Boolean=true\nsys.component.S2TBX\$Boolean=false\nsys.component.S3TBX\$Boolean=false\nsys.component.SNAP\$Boolean=true\nsys.installationDir=$(pwd)/snap\nsys.languageId=en\nsys.programGroupDisabled\$Boolean=false\nsys.symlinkDir=/usr/local/bin" > snap.varfile
+    echo -e "deleteAllSnapEngineDir\$Boolean=false\ndeleteOnlySnapDesktopDir\$Boolean=false\nexecuteLauncherWithPythonAction\$Boolean=false\nforcePython\$Boolean=false\npythonExecutable=/usr/bin/python\nsys.adminRights\$Boolean=true\nsys.component.RSTB\$Boolean=true\nsys.component.S1TBX\$Boolean=true\nsys.component.S2TBX\$Boolean=false\nsys.component.S3TBX\$Boolean=false\nsys.component.SNAP\$Boolean=true\nsys.installationDir=${SNAP_DIR}\nsys.languageId=en\nsys.programGroupDisabled\$Boolean=false\nsys.symlinkDir=/usr/local/bin" > snap.varfile
     echo 'Installing SNAP 13...'
     ./esa-snap_sentinel_linux-13.0.0.sh -q -varfile "$(pwd)/snap.varfile" -dir "${SNAP_DIR}"
     echo 'Configuring SNAP memory settings...'
-    echo "-Xmx16G" > "/Data_large/SARGFM/snap13/snap/bin/gpt.vmoptions"
+    echo "-Xmx16G" > "${SNAP_DIR}/snap/bin/gpt.vmoptions"
     echo 'SNAP 13 installation complete.'
 
 else
